e3c2d5703643034ba447312d84e46468c3207607
AwaisIjaz <Awais_ijaz786@hotmail.com>
2018-04-25 17:16:16 +0100

    Able to add date to table via postman and h2console

---
+++ src/main/java/com/qa/interoperability/MovieEndpoint.java
@@ -0,0 +1,27 @@
+package com.qa.interoperability;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.qa.business.repository.IMovieRepository;
+import com.qa.business.service.IMovieService;
+import com.qa.business.service.MovieService;
+
+@Path("/movie")
+public class MovieEndpoint {
+
+	
+
+	@Inject
+	private IMovieService service;
+
+	@Path("/json")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllMovies() {
+
+		return service.getAllMovies();
+	}
+}
